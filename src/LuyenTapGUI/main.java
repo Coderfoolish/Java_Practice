df5ae179f478e33a89077a4ad244f16da61f5d67
Dino? <daophuduytan2203@gmail.com>
2024-02-06 23:08:26 +0700

    ngay 6/3

--- src/LuyenTapGUI/main.java
+++ src/LuyenTapGUI/main.java
@@ -1,6 +1,10 @@
 package LuyenTapGUI;
 
-public class main {
+import javax.swing.*;
+
+public class main extends JFrame {
     public static void main(String[] args) {
+        JButton btn1 = new JButton();
+
     }
 }
